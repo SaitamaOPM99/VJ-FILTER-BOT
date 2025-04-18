34565acd113dd11ed29cf2a23cbf99f1c7e3d98f
Felix K Friju <66660551+SaitamaOPM99@users.noreply.github.com>
2025-04-18 15:26:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+FbXP7ElKdiUyM2I1 https://t.me/+OTxYa1KcyENiYTJl')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+FbXP7ElKdiUyM2I1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/AnimeXSaga')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'CrewXSaga') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/MalluSaitama')
